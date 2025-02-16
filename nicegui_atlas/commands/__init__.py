e6d48103f08273e393445c0e87c94f32a4cd8a5b
Michael Ikemann <michael.ikemann@gmail.com>
2025-02-16 13:12:57 +0100

    Removed NiceGUI verify functions

--- nicegui_atlas/commands/__init__.py
+++ nicegui_atlas/commands/__init__.py
@@ -1,6 +1,6 @@
 """Command plugins for NiceGUI Atlas."""
 
 from .base import registry, CommandPlugin
-from . import info, index, verify, build, qinfo, backup
+from . import info, index, build, qinfo, backup
 
 __all__ = ['registry', 'CommandPlugin']
